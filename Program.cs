539bc6cdee15110e4cc56a0321e811b58bb56b88
Augusto Nascimento <134967419+xcnoris@users.noreply.github.com>
2024-02-02 18:16:27 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using ScreenSound.Menus;
+using ScreenSound.Modelos;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Banda ira = new Banda("Ira!");
+        ira.AdicionarNota(new Avaliacao(10));
+        ira.AdicionarNota(new Avaliacao(8));
+        ira.AdicionarNota(new Avaliacao(6));
+        Banda beatles = new("The beatles");
+
+        Dictionary<string, Banda> bandasRegistradas = new();
+        bandasRegistradas.Add(ira.Nome, ira);
+        bandasRegistradas.Add(beatles.Nome, beatles);
+
+        void ExibirLogo()
+        {
+            Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+            Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
+        };
+
+        void ExibirOpcoesDoMenu()
+        {
+            ExibirLogo();
+            Console.WriteLine("\nDigite 1 para registrar uma banda");
+            Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+            Console.WriteLine("Digite 3 para mostrar todas as bandas");
+            Console.WriteLine("Digite 4 para avaliar uma banda");
+            Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+            Console.WriteLine("Digite -1 para sair");
+
+            Console.Write("\nDigite a sua opção: ");
+            string opcaoEscolhida = Console.ReadLine()!;
+            int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+            switch (opcaoEscolhidaNumerica)
+            {
+                case 1:
+                    MenuRegistrarBanda menu1 = new();
+                    menu1.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
+                    break;
+                case 2:
+                    MenuRegistrarAlbum menu2 = new();
+                    menu2.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
+                    break;
+                case 3:
+                    MenuMostrarBandasRegistradas menu3 = new();
+                    menu3.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
+                    break;
+                case 4:
+                    MenuAvaliarBanda menu4 = new();
+                    menu4.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
+                    break;
+                case 5:
+                    MenuExibirDetalhes menu5 = new();
+                    menu5.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
+                    break;
+                case -1:
+                    Console.WriteLine("Tchau tchau :)");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+
+    }
+}
